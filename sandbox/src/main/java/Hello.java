1461575c179e93d8109af9dcef22b966c02369bc
Alexander <vsepo356@mail.ru>
2020-04-11 01:51:41 +0300

    создан проект песочница

---
+++ sandbox/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("I ride ");
+        System.out.print("the world ");
+        System.out.println("and I love my little son David!");
+
+    }
+
+}
+	
