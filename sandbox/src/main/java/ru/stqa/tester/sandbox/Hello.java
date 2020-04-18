4328ab78fa2768467cead0d5a07b5bc49aef6faa
Alexander <vsepo356@mail.ru>
2020-04-18 20:22:05 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/tester/sandbox/Hello.java
+++ sandbox/src/main/java/ru/stqa/tester/sandbox/Hello.java
@@ -2,10 +2,40 @@ package ru.stqa.tester.sandbox;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("I ride ");
-        System.out.print("the world ");
-        System.out.println("and I love my little son David!");
+        ride();
+        world();
+        David("David");
 
+        ride();
+        world();
+        David("Abramchik");
+
+        ride();
+        world();
+        David("Iosik");
+
+        double l = 10;
+      System.out.println("Площадь квадрата со стороной " + l + " = " + ploshad(l));
+
+      double a = 5;
+      double b = 10;
+      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + pryamougolnik(a,b));
+    }
+    public static void ride(){
+      System.out.print("I ride ");
+    }
+    public static void world(){
+    System.out.print("the world ");
+  }
+    public static void David (String somebody){
+      System.out.println("and I love my little son " +  somebody +"!");
+    }
+    public static double ploshad(double len){
+      return len*len;
+
+    }
+    public static double pryamougolnik (double a, double b){
+      return a*b;
     }
 
 }
